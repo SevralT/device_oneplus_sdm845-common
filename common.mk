f703f84ac71f54dcd7821e5b8ec49faf3d37788b
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2022-04-09 21:10:53 +0000

    sdm845-common: Build vendor.qti.hardware.capabilityconfigstore@1.0.vendor F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable

--- common.mk
+++ common.mk
@@ -158,7 +158,8 @@ PRODUCT_PACKAGES += \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
 # Boot control
 PRODUCT_PACKAGES += \
